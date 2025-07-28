bd7d9e8a47ad16eb76307f0bcef1e2b83853d237
prakash0428 <prakash0428msi@gmail.com>
2025-07-28 11:35:30 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,22 @@
+async function analyzeEmotion() {
+    const text = document.getElementById('userText').value.trim();
 
+    if (!text) {
+        alert("Kripya pehle kuch text likhiye!");
+        return;
+    }
+
+    const response = await fetch('/analyze_emotion', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ text: text })
+    });
+
+    const data = await response.json();
+
+    if (data.emotion) {
+        document.getElementById('result').innerText = Emotion: ${data.emotion};
+    } else {
+        document.getElementById('result').innerText = 'Kuch galti ho gayi 😔';
+    }
+}
